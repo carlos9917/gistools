3a32ec9f6a5a54cf451be28af05a4c89771bf121
Carlos Peralta <carlos9917@gmail.com>
2023-08-27 21:31:03 +0200

    Added a new notebook to include all models (meaning R01 and glatmodel)

--- visualization/run_quarto.sh
+++ visualization/run_quarto.sh
@@ -1,4 +1,4 @@
 # This is currently working only on my py39 env
 #quarto preview stations_evaluation.qmd --no-browser --no-watch-inputs
-quarto preview data_distribution.qmd --no-browser --no-watch-inputs
+quarto preview data_distribution_allmodels.qmd --no-browser --no-watch-inputs
 
